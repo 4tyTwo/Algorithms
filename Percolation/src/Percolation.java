0490a077c61ea7a816ce74c7b206f6c40da77fa5
4tyTwo <pwutag33@yandex.ru>
2018-08-26 14:56:04 +0300

    Fixed codestyle and bug, which made openedSites have wrong value, now the score in 94/100. The only problem left is wrong isFull method

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -2,13 +2,13 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-  private WeightedQuickUnionUF uf;
-  private int data[][]; //Represents info about opened/closed data
+  private boolean[][] data; // Represents info about opened/closed data
   private int openedSites;
-  private int size;
-  private int virtualBottom, virtualTop;
+  private final WeightedQuickUnionUF uf;
+  private final int size;
+  private final int virtualBottom, virtualTop;
 
-  public Percolation(int n){
+  public Percolation(int n) {
     if (n <= 0)
       throw new java.lang.IllegalArgumentException("Size can't be 0 or negative");
     size = n;
@@ -16,66 +16,64 @@ public class Percolation {
     virtualTop = (size * size);
     virtualBottom = virtualTop + 1;
     openedSites = 0;
-    data = new int[n][n];
+    data = new boolean[n][n];
     for (int i = 0; i < n; ++i)
       for (int j = 0; j < n; ++j)
-        data[i][j] = 0;
+        data[i][j] = false;
   }
 
-  public void open(int row, int col){
+  public void open(int row, int col) {
     if (!(checkSize(row) && checkSize(col)))
       throw new java.lang.IllegalArgumentException("index is out of boundaries");
-    data[row - 1][col -1] = 1; //Open
-    ++openedSites;
-    uniteAdjustments(row, col);
+    if (!data[row - 1][col - 1]) {
+      data[row - 1][col - 1] = true; // Open
+      ++openedSites;
+      uniteAdjustments(row, col);
+    }
   }
 
-  public boolean isOpen(int row, int col){
+  public boolean isOpen(int row, int col) {
     if (!(checkSize(row) && checkSize(col)))
       throw new java.lang.IllegalArgumentException("index is out of boundaries");
-    return data[row - 1][col -1] == 1;
+    return data[row - 1][col -1];
   }
 
-  public boolean isFull(int row, int col){
-    //Check if connected to virtualTop
+  public boolean isFull(int row, int col) {
+    // Check if connected to virtualTop AND has full neighbour
     if (!(checkSize(row) && checkSize(col)))
       throw new java.lang.IllegalArgumentException("index is out of boundaries");
-    return uf.connected(toArrayIndex(row, col), virtualTop);
+    return  uf.find(toArrayIndex(row, col)) == uf.find(virtualTop);
   }
 
-  public int numberOfOpenSites(){
+  public int numberOfOpenSites() {
     return openedSites;
   }
 
-  public boolean percolates(){
+  public boolean percolates() {
     return uf.connected(virtualBottom, virtualTop);
   }
 
-  private boolean checkSize(int checked){
+  private boolean checkSize(int checked) {
     return (checked > 0 && checked <= size);
   }
 
-  private int toArrayIndex(int row, int col){
+  private int toArrayIndex(int row, int col) {
     return (row - 1) * size + col - 1;
   }
 
-  private void uniteAdjustments(int row, int col){
+  private void uniteAdjustments(int row, int col) {
     int curr = toArrayIndex(row, col);
     if (row == 1)
       uf.union(curr, virtualTop);
     if (row == size)
       uf.union(curr, virtualBottom);
-    if (checkSize(row-1))
-      if (isOpen(row - 1, col))
+    if (checkSize(row-1) && isOpen(row - 1, col))
         uf.union(curr - size, curr);
-    if (checkSize(row + 1))
-      if (isOpen(row + 1, col))
+    if (checkSize(row + 1) && isOpen(row + 1, col))
         uf.union(curr + size, curr);
-    if (checkSize(col - 1))
-      if (isOpen(row, col - 1))
+    if (checkSize(col - 1) && isOpen(row, col - 1))
         uf.union(curr - 1, curr);
-    if (checkSize(col + 1))
-      if (isOpen(row, col + 1))
+    if (checkSize(col + 1) && isOpen(row, col + 1))
         uf.union(curr + 1, curr);
   }
 
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -5,50 +5,53 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-  private double percolations[];
-  private int size, T;
-  public PercolationStats(int n, int trials){
+  private static final double CONFIDENCE_95 = 1.96;
+  private final double mean, stddev, confidenceLo, confidenceHi;
+
+  public PercolationStats(int n, int trials) {
     if (n <= 0 || trials <= 0)
       throw new java.lang.IllegalArgumentException("Arguments can't 0 or negative");
-    T = trials;
-    double numOfElems = (int) Math.pow(n, 2);
-    size = n;
-    percolations = new double[T];
-    for (int i = 0; i < T; ++i){
-      Percolation p = new Percolation(size);
+    double numOfElements = (int) Math.pow(n, 2);
+    double[] percolationData = new double[trials];
+    for (int i = 0; i < trials; ++i) {
+      Percolation p = new Percolation(n);
       int row, col;
       while (!p.percolates()) {
         do {
-          row = 1 + StdRandom.uniform(size);
-          col = 1 + StdRandom.uniform(size);
+          row = 1 + StdRandom.uniform(n);
+          col = 1 + StdRandom.uniform(n);
         } while (p.isOpen(row, col));
         p.open(row, col);
       }
-     percolations[i] = p.numberOfOpenSites() / numOfElems;
+      percolationData[i] = p.numberOfOpenSites() / numOfElements;
     }
+    mean = StdStats.mean(percolationData);
+    if (percolationData.length == 1)
+      stddev = Double.NaN;
+    else
+      stddev = StdStats.stddev(percolationData);
+    confidenceHi = mean + (CONFIDENCE_95 * stddev)/Math.sqrt(percolationData.length);
+    confidenceLo = mean - (CONFIDENCE_95 * stddev)/Math.sqrt(percolationData.length);
+
   }
 
-  public double mean(){
-    return StdStats.mean(percolations);
+  public double mean() {
+    return mean;
   }
 
-  public double stddev(){
-    return StdStats.stddev(percolations);
+  public double stddev() {
+    return stddev;
   }
 
-  public double confidenceLo(){
-    double s = stddev();
-    double mean = mean();
-    return (mean - (1.96 * s)/Math.sqrt(T));
+  public double confidenceLo() {
+    return confidenceLo;
   }
 
-  public double confidenceHi(){
-    double s = stddev();
-    double mean = mean();
-    return (mean + (1.96 * s)/Math.sqrt(T));
+  public double confidenceHi() {
+    return confidenceHi;
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     int n = StdIn.readInt();
     int trials = StdIn.readInt();
     PercolationStats PS = new PercolationStats(n, trials);
